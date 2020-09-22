45043b46e3b95cf010ad804cf5be095da52d2205
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2020-09-22 09:54:16 -0700

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,35 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+result = area(7, 8)
+print(result)
+
+result = area(9, 10)
+print(result)
+
+result = area(11, 12)
+print(result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(5, 6)
+print(result)
+
+result = subtract(7, 8)
+print(result)
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+result = add(5, 6)
+print(result)
+
+result = add(7, 8)
+print(result)
